c1d0327b96555302c6d8225617f04b57c49889b2
Priyadharshanip <dharshanip193@gmail.com>
2025-04-02 14:47:04 +0530

    update

---
+++ Add.py
@@ -0,0 +1,8 @@
+def add_numbers(a, b):
+    return a + b;
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+result = add_numbers(a, b)
+print(f"The sum of {a} and {b} is {result}")
