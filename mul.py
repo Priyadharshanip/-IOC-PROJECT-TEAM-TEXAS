a1665ca5d40d9a2d8d09a3137d5e198431509236
krish-dotcom518 <krishikavenkatesan55@gmail.com>
2025-04-02 15:12:11 +0530

    update

---
+++ mul.py
@@ -0,0 +1,8 @@
+def mul_numbers(a, b):
+    return a * b;
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+result = mul_numbers(a, b)
+print(f"The product of {a} and {b} is {result}")
